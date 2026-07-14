a850ed7cf826eaa22115680cef5a293cc27db374
Firdevs Palay <firdevspalay@icloud.com>
2026-07-14 12:23:30 +0300

    Re-enable email verification

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,8 +1,7 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
@@ -12,7 +11,7 @@ use Illuminate\Notifications\Notifiable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
-class User extends Authenticatable{
+class User extends Authenticatable implements MustVerifyEmail{
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
