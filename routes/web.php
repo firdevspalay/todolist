0c17bd2e6c25b04ec04f39253c803633538c1685
Firdevs Palay <firdevspalay@icloud.com>
2026-07-14 11:35:08 +0300

    Temporarily disable email verification

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
     Route::post('/lists', [TodoListController::class, 'store']) ->name('lists.store');
     Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
